c6f8aab01a5407f187ee5e2b5f943403ccac005d
Shyam Saladi <saladi@caltech.edu>
2017-01-02 22:55:38 -0600

    move files for packaging add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='codonpair',
+      version='1.0a',
+      description='A Python implementation to calculate codon pair score',
+      url='http://github.com/smsaladi/codonpair',
+      author='Shyam Saladi',
+      author_email='saladi@caltech.edu',
+      license='Non-commercial Academic Use License',
+      packages=['cps'],
+      install_requires=['numpy', 'pandas', 'biopython'],
+      package_data={'cps': ['data/*.tbd']},
+      entry_points={'console_scripts': ['cps=cps.cps:main']},
+      zip_safe=True,
+      include_package_data=True)
