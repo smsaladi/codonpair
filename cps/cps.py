7131af7febc64cd2a65a700e2a3ec72f3fa15855
Shyam Saladi <saladi@caltech.edu>
2017-01-04 19:16:04 -0800

    rearrange files. move main into cps.py

--- cps/cps.py
+++ cps/cps.py
@@ -9,12 +9,16 @@ Science, Vol. 320, 1784-1787, 27 June 2008
 Shyam Saladi (saladi@caltech.edu)
 """
 
+import sys
 import warnings
 import re
 from pkg_resources import resource_filename
+import argparse
 
 import pandas as pd
 import numpy as np
+
+import Bio.SeqIO
 import Bio.Data.CodonTable
 
 ref_pair_df = None
@@ -304,3 +308,54 @@ def write_reference(seq_records, out_fn):
     """
     calc_reference(seq_records).to_csv(out_fn, sep='\t')
     return
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Calculate codon pair score/bias.')
+
+    parser.add_argument('fna_filename',
+        metavar='fna_file',
+        type=str,
+        default='-',
+        help='FASTA-formatted nucleotide file of the Coding Sequences'
+             '(stdin by default)')
+
+    parser.add_argument('--ref',
+        metavar='reference_file',
+        type=str,
+        default=None,
+        help='Calculate reference pairs for provided file')
+
+    parser.add_argument('--output',
+        metavar='output_file',
+        type=str,
+        default=sys.stdout,
+        help='Specifies name of file to write to (stdout by default)')
+
+    parser.add_argument('--calc_reference',
+        action='store_true',
+        help='Calculate reference pairs for provided file')
+
+    args = parser.parse_args()
+
+    if args.ref is not None:
+        codon_pair_ref(args.ref)
+
+    if args.fna_filename == '-':
+        print("Reading sequences from stdin", file=sys.stderr)
+        seq_records = Bio.SeqIO.parse(args.fna_file, "fasta")
+    else:
+       with open(args.fna_filename, 'r+') as fh:
+            seq_records = list(Bio.SeqIO.parse(fh, "fasta"))
+
+    if args.calc_reference:
+        write_reference(seq_records, args.output)
+    else:
+        for record in seq_records:
+            print(calc_cpb(str(record.seq)))
+
+
+if __name__ == '__main__':
+    main()
+
