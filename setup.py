b1fb370dd7e0e984cd3ce2af3b8432addacf83dc
Shyam Saladi <smsaladi@gmail.com>
2020-02-20 17:42:36 -0800

    modify version for travis deploy to pypi-test

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 
+import os
 import os.path
 from setuptools import setup
 
@@ -6,8 +7,14 @@ this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+version = '0.1.1'
+# For travis/pypi-test
+if os.environ.get('TRAVIS') == 'true' and os.environ.get('TRAVIS_TAG') == '':
+      N, M = os.environ['TRAVIS_JOB_NUMBER'].split('.')
+      version = "{v}-a{N}.dev{M}".format(v=version, N=N, M=M)
+
 setup(name='codonpair',
-      version='0.1.1',
+      version=version,
       description='A Python implementation to calculate codon pair score',
       url='http://github.com/smsaladi/codonpair',
       author='Shyam Saladi',
